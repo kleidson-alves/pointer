f29a08942afb827ec035c2c14979ba58e698c134
Kleidson Alves <kleidsonalves15@gmail.com>
2023-02-15 11:24:23 -0300

    fix: eslint

--- src/components/HistoryTable/Pagination/index.tsx
+++ src/components/HistoryTable/Pagination/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React from "react";
 import { createList } from "../../../utils/functions/paginationFunctions";
 
 import styles from "./styles.module.scss";
